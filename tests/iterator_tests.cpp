5fad8e49935c11c942efb7313b1f5b1c3cde47f4
Ryan P. Nicholl <rnicholl@protonmail.com>
2026-01-29 19:06:24 -0800

    iterator

---
+++ tests/iterator_tests.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) 2026 Ryan P. Nicholl
+// SPDX-License-Identifier: Apache-2.0
+
+#include "gtest/gtest.h"
+
+#include <vector>
+#include <numeric>
+
+#include "rpnx/iterator.hpp"
+
+TEST(forward_bounded_iterator, basic_traversal_with_neq_only)
+{
+    std::vector<int> v{1,2,3,4,5};
+
+    auto it  = rpnx::make_forward_bounded_iterator(v.begin(), v.end());
+    auto end = rpnx::make_forward_bounded_iterator(v.end(),   v.end());
+
+    int sum = 0;
+    while (it != end) {
+        sum += *it;
+        ++it;
+    }
+    EXPECT_EQ(sum, 15);
+}
+
+TEST(forward_bounded_iterator, deref_at_end_throws)
+{
+    std::vector<int> v{10};
+    auto end = rpnx::make_forward_bounded_iterator(v.end(), v.end());
+    EXPECT_THROW((void)*end, std::out_of_range);
+}
+
+TEST(forward_bounded_iterator, increment_at_end_throws)
+{
+    std::vector<int> v{10};
+    auto end = rpnx::make_forward_bounded_iterator(v.end(), v.end());
+    EXPECT_THROW(++end, std::out_of_range);
+}
